17ede71134e71a6482de4c70a7d8459f7edad850
Jameel Kareem <34586278+jameelkareem@users.noreply.github.com>
2018-05-18 10:19:30 -0500

    Update add-murata-wireless.sh

--- add-murata-layer-script/add-murata-wireless.sh
+++ add-murata-layer-script/add-murata-wireless.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 # Updating local.conf for TI MIRRORS
-echo "TI_MIRROR = \"http://software-dl.ti.com/processor-sdk-mirror/sources/\"" >> conf/local.conf
-echo "MIRRORS += \" \\" >> conf/local.conf
-echo "bzr://.*/.*      \${TI_MIRROR} \n \\" >> conf/local.conf
-echo "cvs://.*/.*      \${TI_MIRROR} \n \\" >> conf/local.conf
-echo "git://.*/.*      \${TI_MIRROR} \n \\" >> conf/local.conf
-echo "gitsm://.*/.*    \${TI_MIRROR} \n \\" >> conf/local.conf
-echo "hg://.*/.*       \${TI_MIRROR} \n \\" >> conf/local.conf
-echo "osc://.*/.*      \${TI_MIRROR} \n \\" >> conf/local.conf
-echo "p4://.*/.*       \${TI_MIRROR} \n \\" >> conf/local.conf
-echo "npm://.*/.*      \${TI_MIRROR} \n \\" >> conf/local.conf
-echo "ftp://.*/.*      \${TI_MIRROR} \n \\" >> conf/local.conf
-echo "https?$://.*/.*  \${TI_MIRROR} \n \\" >> conf/local.conf
-echo "svn://.*/.*      \${TI_MIRROR} \n \\" >> conf/local.conf
-echo "\"" >> conf/local.conf
+#echo "TI_MIRROR = \"http://software-dl.ti.com/processor-sdk-mirror/sources/\"" >> conf/local.conf
+#echo "MIRRORS += \" \\" >> conf/local.conf
+#echo "bzr://.*/.*      \${TI_MIRROR} \n \\" >> conf/local.conf
+#echo "cvs://.*/.*      \${TI_MIRROR} \n \\" >> conf/local.conf
+#echo "git://.*/.*      \${TI_MIRROR} \n \\" >> conf/local.conf
+#echo "gitsm://.*/.*    \${TI_MIRROR} \n \\" >> conf/local.conf
+#echo "hg://.*/.*       \${TI_MIRROR} \n \\" >> conf/local.conf
+#echo "osc://.*/.*      \${TI_MIRROR} \n \\" >> conf/local.conf
+#echo "p4://.*/.*       \${TI_MIRROR} \n \\" >> conf/local.conf
+#echo "npm://.*/.*      \${TI_MIRROR} \n \\" >> conf/local.conf
+#echo "ftp://.*/.*      \${TI_MIRROR} \n \\" >> conf/local.conf
+#echo "https?$://.*/.*  \${TI_MIRROR} \n \\" >> conf/local.conf
+#echo "svn://.*/.*      \${TI_MIRROR} \n \\" >> conf/local.conf
+#echo "\"" >> conf/local.conf
 
 # Updating BBLAYERS with meta-murata-wireless
-echo "BBLAYERS += \" ${TISDK_DIR}/sources/meta-murata-wireless \"" >> conf/bblayers.conf
+#echo "BBLAYERS += \" ${TISDK_DIR}/sources/meta-murata-wireless \"" >> conf/bblayers.conf
 
+echo "INSANE_SKIP_\${PN} += \"dev-deps\"" >> meta-ros/recipes-ros/vision-opencv/cv-bridge_1.11.16.bb
