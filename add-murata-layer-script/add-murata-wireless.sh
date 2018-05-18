122bbf2a47ee359414e343c2921a644cff3c39ce
Jameel Kareem <34586278+jameelkareem@users.noreply.github.com>
2018-05-18 10:36:09 -0500

    Update add-murata-wireless.sh

--- add-murata-layer-script/add-murata-wireless.sh
+++ add-murata-layer-script/add-murata-wireless.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 # Updating local.conf for TI MIRRORS
-#echo "TI_MIRROR = \"http://software-dl.ti.com/processor-sdk-mirror/sources/\"" >> conf/local.conf
-#echo "MIRRORS += \" \\" >> conf/local.conf
-#echo "bzr://.*/.*      \${TI_MIRROR} \n \\" >> conf/local.conf
-#echo "cvs://.*/.*      \${TI_MIRROR} \n \\" >> conf/local.conf
-#echo "git://.*/.*      \${TI_MIRROR} \n \\" >> conf/local.conf
-#echo "gitsm://.*/.*    \${TI_MIRROR} \n \\" >> conf/local.conf
-#echo "hg://.*/.*       \${TI_MIRROR} \n \\" >> conf/local.conf
-#echo "osc://.*/.*      \${TI_MIRROR} \n \\" >> conf/local.conf
-#echo "p4://.*/.*       \${TI_MIRROR} \n \\" >> conf/local.conf
-#echo "npm://.*/.*      \${TI_MIRROR} \n \\" >> conf/local.conf
-#echo "ftp://.*/.*      \${TI_MIRROR} \n \\" >> conf/local.conf
-#echo "https?$://.*/.*  \${TI_MIRROR} \n \\" >> conf/local.conf
-#echo "svn://.*/.*      \${TI_MIRROR} \n \\" >> conf/local.conf
-#echo "\"" >> conf/local.conf
+echo "TI_MIRROR = \"http://software-dl.ti.com/processor-sdk-mirror/sources/\"" >> conf/local.conf
+echo "MIRRORS += \" \\" >> conf/local.conf
+echo "bzr://.*/.*      \${TI_MIRROR} \n \\" >> ${TI_BUILD_DIR}/conf/local.conf
+echo "cvs://.*/.*      \${TI_MIRROR} \n \\" >> ${TI_BUILD_DIR}/conf/local.conf
+echo "git://.*/.*      \${TI_MIRROR} \n \\" >> ${TI_BUILD_DIR}/conf/local.conf
+echo "gitsm://.*/.*    \${TI_MIRROR} \n \\" >> ${TI_BUILD_DIR}/conf/local.conf
+echo "hg://.*/.*       \${TI_MIRROR} \n \\" >> ${TI_BUILD_DIR}/conf/local.conf
+echo "osc://.*/.*      \${TI_MIRROR} \n \\" >> ${TI_BUILD_DIR}/conf/local.conf
+echo "p4://.*/.*       \${TI_MIRROR} \n \\" >> ${TI_BUILD_DIR}/conf/local.conf
+echo "npm://.*/.*      \${TI_MIRROR} \n \\" >> ${TI_BUILD_DIR}/conf/local.conf
+echo "ftp://.*/.*      \${TI_MIRROR} \n \\" >> ${TI_BUILD_DIR}/conf/local.conf
+echo "https?$://.*/.*  \${TI_MIRROR} \n \\" >> ${TI_BUILD_DIR}/conf/local.conf
+echo "svn://.*/.*      \${TI_MIRROR} \n \\" >> ${TI_BUILD_DIR}/conf/local.conf
+echo "\"" >> conf/local.conf
+
+# Updating BBLAYERS with meta-murata-wireless
+echo "BBLAYERS += \" ${TISDK_DIR}/sources/meta-murata-wireless \"" >> ${TI_BUILD_DIR}/conf/bblayers.conf
+
 
 # Updating BBLAYERS with meta-murata-wireless
 #echo "BBLAYERS += \" ${TISDK_DIR}/sources/meta-murata-wireless \"" >> ${TISDK_DIR}/conf/bblayers.conf
